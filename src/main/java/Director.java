8f40a4a6d8c07b30ded0b33e8903a4c73131baf0
Administrator <Administrator@STUDENT32>
2017-01-26 15:58:31 +0000

    first push

---
+++ src/main/java/Director.java
@@ -0,0 +1,72 @@
+
+public class Director {
+
+	int director_id;
+	String first_name;
+	String surname;
+	String gender;
+	int dateOfBirth;
+	String nationality;
+	String bio;
+
+	//constructor
+	public Director(){
+		
+	}
+	
+	public int getDirector_id() {
+		return director_id;
+	}
+
+	public void setDirector_id(int director_id) {
+		this.director_id = director_id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(int dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+}
