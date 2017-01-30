ec14e1a4bbed825ca902f8c6d7c8b7e7cd093947
Andrew Hirst <andy.hirst95@gmail.com>
2017-01-30 14:52:00 +0000

    QC-8 Create the movie entity, updated movie entity.

--- src/main/java/com/qa/student/model/Movie.java
+++ src/main/java/com/qa/student/model/Movie.java
@@ -1,44 +1,104 @@
 package com.qa.student.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import java.util.Date;
 
 @Entity
 public class Movie {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long film_id;
 	
-	//ID's below need foreign keys implementing
-	@NotNull
-	private Long director_id;
+	//COMMENTED OUT CODE IS POSSIBLE IMPLEMENTATION FOR RELATIONS BETWEEN FILM, DIRECTORS AND ACTORS
+	/*
+	@Column(nullable=false)
+	@ManyToMany(cascade= CascadeType.ALL)
+	@JoinTable(name="Film_Director_Join",joinColumns =
+	{@JoinColumn(name ="film_id",nullable = false)},
+	inverseJoinColumns = {@JoinColumn(name = "director_id", nullable = false)})
+	private Director director;
 	
-	@NotNull
-	private Long actor_id;
+
+	@Column(nullable=false)
+	@ManyToMany(cascade= CascadeType.ALL)
+	@JoinTable(name="Film_Actor_Join",joinColumns =
+	{@JoinColumn(name ="film_id",nullable = false)},
+	inverseJoinColumns = {@JoinColumn(name = "actor_id", nullable = false)})
+	private Actor actor;
+	*/
 	
-	@NotNull
+	@Column(nullable = false, length = 100)
+	@Size(max = 100)
 	private String title;
 	
-	@NotNull
+	@Column(nullable = false, length = 100)
+	@Size(max = 100)
 	private String genre;
 	
+	@Column(length = 500)
+	@Size(max = 500)
 	private String description;
 	
-	@NotNull
+	@Column(nullable = false, length = 20)
+	@Size(max = 20)
 	private String certification;
 	
-	@NotNull
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
 	private Date releaseDate;
 	
-	@NotNull
+	@Column(nullable = false)
 	private int duration;
 	
-	@NotNull
+	@Column(nullable = false, length = 30)
+	@Size(max = 30)
 	private String language;
+	
+	public Movie()
+	{
+		
+	}
+	
+	public Movie(Long film_id, String title, String genre, String description,
+			String certification, Date releaseDate, int duration,
+			String language) {
+		super();
+		this.film_id = film_id;
+		this.title = title;
+		this.genre = genre;
+		this.description = description;
+		this.certification = certification;
+		this.releaseDate = releaseDate;
+		this.duration = duration;
+		this.language = language;
+	}
+	
+	public Movie(Long film_id, String title, String genre,
+			String certification, Date releaseDate, int duration,
+			String language) {
+		super();
+		this.film_id = film_id;
+		this.title = title;
+		this.genre = genre;
+		this.certification = certification;
+		this.releaseDate = releaseDate;
+		this.duration = duration;
+		this.language = language;
+	}
 
 	public Long getFilm_id() {
 		return film_id;
@@ -46,22 +106,6 @@ public class Movie {
 
 	public void setFilm_id(Long film_id) {
 		this.film_id = film_id;
-	}
-
-	public Long getDirector_id() {
-		return director_id;
-	}
-
-	public void setDirector_id(Long director_id) {
-		this.director_id = director_id;
-	}
-
-	public Long getActor_id() {
-		return actor_id;
-	}
-
-	public void setActor_id(Long actor_id) {
-		this.actor_id = actor_id;
 	}
 
 	public String getTitle() {
@@ -119,5 +163,80 @@ public class Movie {
 	public void setLanguage(String language) {
 		this.language = language;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Movie [film_id=" + film_id + ", title=" + title + ", genre="
+				+ genre + ", description=" + description + ", certification="
+				+ certification + ", releaseDate=" + releaseDate
+				+ ", duration=" + duration + ", language=" + language + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((certification == null) ? 0 : certification.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + duration;
+		result = prime * result + ((film_id == null) ? 0 : film_id.hashCode());
+		result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+		result = prime * result
+				+ ((language == null) ? 0 : language.hashCode());
+		result = prime * result
+				+ ((releaseDate == null) ? 0 : releaseDate.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		if (certification == null) {
+			if (other.certification != null)
+				return false;
+		} else if (!certification.equals(other.certification))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (duration != other.duration)
+			return false;
+		if (film_id == null) {
+			if (other.film_id != null)
+				return false;
+		} else if (!film_id.equals(other.film_id))
+			return false;
+		if (genre == null) {
+			if (other.genre != null)
+				return false;
+		} else if (!genre.equals(other.genre))
+			return false;
+		if (language == null) {
+			if (other.language != null)
+				return false;
+		} else if (!language.equals(other.language))
+			return false;
+		if (releaseDate == null) {
+			if (other.releaseDate != null)
+				return false;
+		} else if (!releaseDate.equals(other.releaseDate))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
 }
