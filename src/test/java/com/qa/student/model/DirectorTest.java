f262a378c276254f8b26e111e08c94e181df6268
Administrator <Administrator@STUDENT32>
2017-01-30 17:00:00 +0000

    unit tests completed!

--- src/test/java/com/qa/student/model/DirectorTest.java
+++ src/test/java/com/qa/student/model/DirectorTest.java
@@ -1,21 +1,30 @@
 package com.qa.student.model;
 
+import static org.junit.Assert.*;
+
 import org.junit.Assert.*;
 import org.junit.Test;
 
 public class DirectorTest {	
 	
-//	@Test
-//	public void nameTest() {
-//		Director dir = new Director();
-//		dir.setDFirst_name(1);
-//		assertEquals(dir.getDirector_id, 1);
-//		}
+	@Test
+	public void firstNameTest() {
+		Director dir = new Director();
+		dir.setFirst_name("testName");
+		assertEquals(dir.getFirst_name(), "testName");
+		}
 	
 	@Test
 	public void idTest() {
 		Director dir = new Director();
 		dir.setDirector_id(1);
-		assertEquals(dir.getDirector_id, 1);
+		assertEquals(dir.getDirector_id(), 1);
+		}
+	
+	@Test
+	public void lastNameTest() {
+		Director dir = new Director();
+		dir.setSurname("surnameExample");
+		assertEquals(dir.getSurname(), "surnameExample");
 		}
 }
