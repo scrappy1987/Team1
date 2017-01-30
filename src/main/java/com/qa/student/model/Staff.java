0e7359065a024ea3bc92e8317f5370f2eca1b308
russellc92 <russell_claxton@hotmail.com>
2017-01-30 17:04:48 +0000

    QC-13-Create-the-staff-entity

--- src/main/java/com/qa/student/model/Staff.java
+++ src/main/java/com/qa/student/model/Staff.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 
 @Entity
 public class Staff {
-	
+
 	@Id
 	@GeneratedValue
 	private int idPerson;
@@ -18,26 +18,35 @@ public class Staff {
 	@NotNull
 	@Column(name = "Job_Role")
 	private String jobRole;
-	
-    @OneToOne 
-    @MapsId
-	Person staffPerson;
-	   
-    
-    public Staff(){}
 
-    public Staff(String jobRole, Person staffPerson) {
+	/* @OneToOne 
+    @MapsId
+	Person staffPerson;*/
+
+	public Staff(){}
+
+	public Staff(int idPerson, String jobRole) {
+		super();
+		this.idPerson = idPerson;
+		this.jobRole = jobRole;
+	}
+	public Staff( String jobRole) {
+		super();
+		this.jobRole = jobRole;
+	}
+
+	/*  public Staff(String jobRole, Person staffPerson) {
 		super();
 		this.jobRole = jobRole;
 		this.staffPerson = staffPerson;
 	}
-    
+
 	public Staff(int idPerson, String jobRole, Person staffPerson) {
 		super();
 		this.idPerson = idPerson;
 		this.jobRole = jobRole;
 		this.staffPerson = staffPerson;
-	}
+	}*/
 
 	public int getIdPerson() {
 		return idPerson;
@@ -54,19 +63,49 @@ public class Staff {
 	public void setJobRole(String jobRole) {
 		this.jobRole = jobRole;
 	}
+	
 
-	public Person getStaffPerson() {
+	@Override
+	public String toString() {
+		return "Staff [idPerson=" + idPerson + ", jobRole=" + jobRole + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idPerson;
+		result = prime * result + ((jobRole == null) ? 0 : jobRole.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Staff other = (Staff) obj;
+		if (idPerson != other.idPerson)
+			return false;
+		if (jobRole == null) {
+			if (other.jobRole != null)
+				return false;
+		} else if (!jobRole.equals(other.jobRole))
+			return false;
+		return true;
+	}
+	/*	public Person getStaffPerson() {
 		return staffPerson;
 	}
 
 	public void setStaffPerson(Person staffPerson) {
 		this.staffPerson = staffPerson;
-	}
+	}*/
 
-	@Override
+	/*@Override
 	public String toString() {
 		return "Staff [idPerson=" + idPerson + ", jobRole=" + jobRole
 				+ ", staffPerson=" + staffPerson + "]";
-	}	
-	
+	}	*/
 }
