d2f6cf0c6403117b5b9dbf342a397d920b769cb3
Ian Mannion <manyon05@outlook.com>
2017-01-30 14:37:31 +0000

    QC-12-Create-the-Cinema-Entity

--- src/main/java/com/qa/student/model/Cinema.java
+++ src/main/java/com/qa/student/model/Cinema.java
@@ -1,34 +1,44 @@
 package com.qa.student.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "Cinema")
 public class Cinema {
 
+	@Column(nullable = false, length = 225)
+	@Size(max = 225)
 	@NotNull
 	private String name;
 
 	@Id
+	@Column(nullable = false, unique = true)
 	@GeneratedValue
-	private int addressId;
+	private int Id;
 
+	@Column(nullable = true, length = 225)
+	@Size(max = 225)
 	private String screens;
 
+	@Column(nullable = true)
 	private int capacity;
 
 	public Cinema() {
 
 	}
 
-	public int getaddressId() {
-		return addressId;
+	public int getCinemaId() {
+		return Id;
 	}
 
-	public void setaddressId(int addressId) {
-		this.addressId = addressId;
+	public void setCinemaId(int Id) {
+		this.Id = Id;
 	}
 
 	public String getName() {
@@ -54,6 +64,48 @@ public class Cinema {
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
-}
 
-//heyyyaaaaa
+	@Override
+	public String toString() {
+		return "Cinema [name=" + name + ", Id=" + Id + ", screens=" + screens
+				+ ", capacity=" + capacity + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Id;
+		result = prime * result + capacity;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((screens == null) ? 0 : screens.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cinema other = (Cinema) obj;
+		if (Id != other.Id)
+			return false;
+		if (capacity != other.capacity)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (screens == null) {
+			if (other.screens != null)
+				return false;
+		} else if (!screens.equals(other.screens))
+			return false;
+		return true;
+	}
+
+}
