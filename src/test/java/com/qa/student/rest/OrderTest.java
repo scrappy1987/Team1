26038f1059fb86329bf96733865741ca70d26cfc
Aaron <aaronseymour369@gmail.com>
2017-01-31 15:13:23 +0000

    QC-31: Changing the committer from Example to myself (Aaron)

---
+++ src/test/java/com/qa/student/rest/OrderTest.java
@@ -0,0 +1,12 @@
+package com.qa.student.rest;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class OrderTest {
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
