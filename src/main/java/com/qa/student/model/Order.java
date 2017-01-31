8ffc6a370d21da0488ef77e82954cbd116c5f6cb
Aaron <aaronseymour369@gmail.com>
2017-01-31 16:46:31 +0000

    QC-31: OrderTest Complete

--- src/main/java/com/qa/student/model/Order.java
+++ src/main/java/com/qa/student/model/Order.java
@@ -10,7 +10,7 @@ public class Order {
 	
 	@Id
 	@GeneratedValue
-	private Long customerId;
+	private Long id;
 	
 	@NotNull
 	private Long ticketId;
@@ -18,15 +18,15 @@ public class Order {
 	private Double totalCost;
 	@NotNull
 	private Date orderDate;
-	
+	 
 	public Order() {
 
 	}
-	public Long getCustomerId() {
-		return customerId;
+	public Long getId() {
+		return id;
 	}
-	public void setCustomerId(Long customerId) {
-		this.customerId = customerId;
+	public void setId(Long id) {
+		this.id = id;
 	}
 	public Long getTicketId() {
 		return ticketId;
@@ -46,4 +46,59 @@ public class Order {
 	public void setOrderDate(Date orderDate) {
 		this.orderDate = orderDate;
 	}
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", ticketId=" + ticketId
+				+ ", totalCost=" + totalCost + ", orderDate=" + orderDate + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((orderDate == null) ? 0 : orderDate.hashCode());
+		result = prime * result
+				+ ((ticketId == null) ? 0 : ticketId.hashCode());
+		result = prime * result
+				+ ((totalCost == null) ? 0 : totalCost.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (orderDate == null) {
+			if (other.orderDate != null)
+				return false;
+		} else if (!orderDate.equals(other.orderDate))
+			return false;
+		if (ticketId == null) {
+			if (other.ticketId != null)
+				return false;
+		} else if (!ticketId.equals(other.ticketId))
+			return false;
+		if (totalCost == null) {
+			if (other.totalCost != null)
+				return false;
+		} else if (!totalCost.equals(other.totalCost))
+			return false;
+		return true;
+	}
+	
 }
+
+
+
+
